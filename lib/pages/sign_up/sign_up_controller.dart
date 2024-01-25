7445979a8c08aaf044a9f949ba88a38159e67346
AdeelWajid <adeelwajidbn@gmail.com>
2024-01-26 01:35:34 +0500

    a

--- lib/pages/sign_up/sign_up_controller.dart
+++ lib/pages/sign_up/sign_up_controller.dart
@@ -1,6 +1,8 @@
-
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:river_pod/common/global_loader/global_loader.dart';
 import 'package:river_pod/common/widgets/pop_messages.dart';
 import 'package:river_pod/pages/sign_up/notifier/register_notifier.dart';
 
@@ -9,7 +11,7 @@ class SignUpController {
 
   SignUpController({required this.ref});
 
-  void handleSignUp(){
+  void handleSignUp()async{
     var state = ref.read(registerNotifierProvider);
 
     String name = state.userName;
@@ -17,13 +19,47 @@ class SignUpController {
     String password = state.password;
     String rePassword = state.rePassword;
 
-    print("Name is :${name}");
-    print("Name is :${email}");
-    print("Name is :${password}");
-    print("Name is :${rePassword}");
+    if(name.isEmpty){
+      toastInfo(msg: "Your name is empty");
+      return;
+    }
+    if(email.isEmpty){
+      toastInfo(msg: "your email is empty");
+      return;
+    }
+    if(password.isEmpty){
+      toastInfo(msg: "your password is empty");
+      return;
+    }
 
     if(password != rePassword){
       toastInfo(msg: "Password did not match!");
+      return;
     }
+
+    //show the loading
+    ref.read(appLoaderProvider.notifier).setLoaderValue(true);
+    var context = Navigator.of(ref.context);
+    try{
+
+      final credential=
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      if (kDebugMode) {
+        print(credential);
+      }
+      if(credential.user!=null){
+        await credential.user?.sendEmailVerification();
+        await credential.user?.updateDisplayName(name);
+        // get server photo url
+        // set user photo url
+        toastInfo(msg: "An email has been sent to verify your account.");
+        context.pop();
+      }
+    }catch(e){
+      toastInfo(msg: e.toString());
+    }
+    // dismiss the loading
+    ref.read(appLoaderProvider.notifier).setLoaderValue(false);
   }
 }
